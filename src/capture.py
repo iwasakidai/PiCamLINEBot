1d8d54168e81d831b4d81fce4dd56454097cde44
Dai Iwasaki <dai@tristan.jp>
2025-01-23 01:40:37 +0900

    enable to capture a picture.

--- src/capture.py
+++ src/capture.py
@@ -1,4 +1,7 @@
-def capture_photo(filename = "photo.jpg"):
+from picamera2 import Picamera2, Preview
+import time
+
+def capture_photo(file_path = "photo.jpg"):
     """
     Raspberry Piに設置のカメラで撮影して画像データのパスを返す
 
@@ -7,4 +10,14 @@ def capture_photo(filename = "photo.jpg"):
     Returns:
         None
     """
+    picam2 = Picamera2()
+    camera_config = picam2.create_still_configuration()
+    picam2.configure(camera_config)
+    picam2.start_preview(Preview.NULL)
+
+    picam2.start()
+    time.sleep(2)
+    picam2.capture_file(file_path)
+    picam2.close()
+
     return
